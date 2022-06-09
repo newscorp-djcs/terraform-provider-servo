a7ca41fdd00783cde4b3793e6f77de378ec7243a
Fred De la Pascua <federico.gutierrez@federicogutierrez.local>
2022-06-09 16:47:51 -0500

    creates app no retrieves

--- servo/resource_app.go
+++ servo/resource_app.go
@@ -2,30 +2,13 @@ package servo
 
 import (
 	"context"
-	"net/http"
 	"os"
 
-	// cError "github.com/coreos/etcd/error"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 
-	// "gopkg.in/resty.v1"
 	"terraform-provider-servo/client"
 )
-
-var Token string
-
-type Client struct {
-	HostURL    string
-	HTTPClient *http.Client
-	Token      string
-}
-
-// type App struct {
-// 	// ID     int    `json:"id,omitempty"`
-// 	Handle string `json:"handle,omitempty"`
-// 	Source string `json:"source,omitempty"`
-// }
 
 func resourceApp() *schema.Resource {
 	return &schema.Resource{
@@ -60,16 +43,10 @@ func resourceApp() *schema.Resource {
 			"region": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: true,
-				// Elem: &schema.Schema{
-				// 	Type: schema.TypeString,
-				// },
 			},
 			"org": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: true,
-				// Elem: &schema.Schema{
-				// 	Type: schema.TypeString,
-				// },
 			},
 		},
 	}
@@ -165,74 +142,3 @@ func resourceAppDelete(ctx context.Context, d *schema.ResourceData, m interface{
 
 	return diags
 }
-
-// func (c *Client) CreateApp(newApp App, Token string) (*AppsRes, error) {
-// 	rb, err := json.Marshal(newApp)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	req, err := http.NewRequest("POST", fmt.Sprintf("%sorgs/dev/regions/virginia/apps", "https://next.onservo.com/api"), strings.NewReader(string(rb)))
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	req.Header.Set("token", Token)
-
-// 	// body, err := c.doRequest(req, Token)
-// 	//---
-// 	res, err := c.HTTPClient.Do(req)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	defer res.Body.Close()
-
-// 	body, err := ioutil.ReadAll(res.Body)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	if res.StatusCode != http.StatusOK {
-// 		return nil, fmt.Errorf("status: %d, body: %s", res.StatusCode, body)
-// 	}
-
-// 	//---
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	app := AppsRes{}
-// 	err = json.Unmarshal(body, &app)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	return &app, nil
-// }
-
-// func (c *Client) doRequest(req *http.Request, Token string) ([]byte, error) {
-// 	token := Token
-
-// 	// if authToken != nil {
-// 	// 	token = *authToken
-// 	// }
-
-// 	req.Header.Set("token", token)
-
-// 	res, err := c.HTTPClient.Do(req)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	defer res.Body.Close()
-
-// 	body, err := ioutil.ReadAll(res.Body)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	if res.StatusCode != http.StatusOK {
-// 		return nil, fmt.Errorf("status: %d, body: %s", res.StatusCode, body)
-// 	}
-
-// 	return body, err
-// }
